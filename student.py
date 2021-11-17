0c7cc988628bcdc8ae25cb69a150a6fc6584b21f
TrinaChang <ks20342@gmail.com>
2021-11-17 12:22:13 +0800

    add files

---
+++ student.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+student.py
+
+UNSW COMP9444 Neural Networks and Deep Learning
+
+You may modify this file however you wish, including creating additional
+variables, functions, classes, etc., so long as your code runs with the
+hw2main.py file unmodified, and you are only using the approved packages.
+
+You have been given some default values for the variables train_val_split,
+batch_size as well as the transform function.
+You are encouraged to modify these to improve the performance of your model.
+
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+"""
+   Answer to Question:
+
+Briefly describe how your program works, and explain any design and training
+decisions you made along the way.
+"""
+
+############################################################################
+######     Specify transform(s) to be applied to the input images     ######
+############################################################################
+def transform(mode):
+    """
+    Called when loading the data. Visit this URL for more information:
+    https://pytorch.org/vision/stable/transforms.html
+    You may specify different transforms for training and testing
+    """
+    if mode == 'train':
+        return transforms.ToTensor()
+    elif mode == 'test':
+        return transforms.ToTensor()
+
+
+############################################################################
+######   Define the Module to process the images and produce labels   ######
+############################################################################
+class Network(nn.Module):
+
+    def __init__(self):
+        super(Network, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 5)
+        self.conv2 =  nn.Conv2d(64, 32, 5)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        return out 
+
+    # def __init__(self):
+    #     super().__init__()
+        
+    # def forward(self, input):
+    #     pass
+
+net = Network()
+    
+############################################################################
+######      Specify the optimizer and loss function                   ######
+############################################################################
+optimizer = optim.SGD(net.parameters(), lr=0.005, weight_decay=0.0001, momentum=0)
+
+loss_func = nn.NLLLoss()
+
+
+############################################################################
+######  Custom weight initialization and lr scheduling are optional   ######
+############################################################################
+
+# Normally, the default weight initialization and fixed learing rate
+# should work fine. But, we have made it possible for you to define
+# your own custom weight initialization and lr scheduler, if you wish.
+def weights_init(m):
+    return
+
+scheduler = None
+
+############################################################################
+#######              Metaparameters and training options              ######
+############################################################################
+dataset = "./data"
+train_val_split = 0.8
+batch_size = 200
+epochs = 10

9b0afb05d48f7e15087ba4b9483ea0c660ce0e5d
TrinaChang <ks20342@gmail.com>
2021-11-17 14:09:46 +0800

    trans

--- student.py
+++ student.py
@@ -59,6 +59,22 @@ def transform(mode):
         # return transforms.ToTensor()
         return testSet
 
+class Residual(nn.Module):
+    def __init__(self, input, out):
+        super(Residual, self).__init__()
+        self.relu = nn.ReLU()
+
+        self.conv1 = nn.Conv2d(input, out, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out)
+
+        self.conv2 = nn.Conv2d(out, out, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out)
+
+    def forward(self, out):
+        out = self.relu(self.bn1(self.conv1(out)))
+        out = self.relu(self.bn2(self.conv2(out)))
+        return out
+
 
 ############################################################################
 ######   Define the Module to process the images and produce labels   ######
@@ -67,16 +83,36 @@ class Network(nn.Module):
 
     def __init__(self):
         super(Network, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, 5)
-        self.conv2 =  nn.Conv2d(64, 32, 5)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.relu = nn.ReLU()
+        self.mp = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+
+        self.conv2x =  nn.Sequential(Residual(64, 64), Residual(64, 128))
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3x =  nn.Sequential(Residual(128, 128), Residual(128, 256))
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4x =  nn.Sequential(Residual(256, 256), Residual(256, 256))
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout2d(p=0.5, inplace=True)
+        self.fc1 = nn.Linear(256, 8)
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
-        out = F.max_pool2d(out, 2)
-        out = F.relu(self.conv2(out))
-        out = F.max_pool2d(out, 2)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.mp(out)
+        out = self.conv2x(out)
+        out = self.bn2(out)
+        out = self.conv3x(out)
+        out = self.bn3(out)
+        out = self.conv4x(out)
+        out = self.avgpool(out)
+        out = self.dropout(out)
         out = out.view(out.size(0), -1)
-        return out 
+        out = self.fc1(out)
+        return F.log_softmax(out, dim=1)
 
     # def __init__(self):
     #     super().__init__()
@@ -102,14 +138,16 @@ loss_func = nn.CrossEntropyLoss()
 # should work fine. But, we have made it possible for you to define
 # your own custom weight initialization and lr scheduler, if you wish.
 def weights_init(m):
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out')
     return
 
-scheduler = None
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1)
 
 ############################################################################
 #######              Metaparameters and training options              ######
 ############################################################################
 dataset = "./data"
 train_val_split = 0.8
-batch_size = 200
+batch_size = 64
 epochs = 20

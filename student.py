4abdaeb56349e2c572741528b95078a1c6f79c49
TrinaChang <ks20342@gmail.com>
2021-11-17 16:40:56 +0800

    mod

--- student.py
+++ student.py
@@ -143,9 +143,9 @@ net = Network()
 ############################################################################
 ######      Specify the optimizer and loss function                   ######
 ############################################################################
-optimizer = optim.SGD(net.parameters(), lr=0.005, weight_decay=0.0001, momentum=0)
+optimizer = optim.Adam(net.parameters(), lr=0.005, weight_decay=0.0001)
 
-loss_func = nn.NLLLoss()
+loss_func = nn.CrossEntropyLoss()
 
 
 ############################################################################

2b7e77bcd688900402940f24ccb47ee910c12b52
TrinaChang <ks20342@gmail.com>
2021-11-19 02:17:27 +0800

    kernel size maxpool2

--- student.py
+++ student.py
@@ -39,7 +39,7 @@ def transform(mode):
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomVerticalFlip(p=0.05),
             transforms.RandomRotation(degrees=60),
-            transforms.ToTensor(),
+            transforms.ToTensor()
         ])
         # return transforms.ToTensor()
         return trainSet
@@ -51,7 +51,7 @@ def transform(mode):
             transforms.RandomHorizontalFlip(p=0.5),
             transforms.RandomVerticalFlip(p=0.05),
             transforms.RandomRotation(degrees=60),
-            transforms.ToTensor(),
+            transforms.ToTensor()
         ])
         # return transforms.ToTensor()
         return testSet
@@ -66,10 +66,9 @@ class ResidualBlock(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, stride=stride, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(planes, planes, stride=1, kernel_size=3, padding=1)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.bn = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
-        self.dropout = nn.Dropout2d(0.15)
+        self.dropout = nn.Dropout2d(0.3, inplace=True)
         self.downsample = downsample
         
     def forward(self, x):
@@ -93,13 +92,11 @@ class Network(nn.Module):
     def __init__(self):
         super().__init__()
         self.inplanes = 64  # if change this, change first arg of self.l1 to the same value
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=10, stride=2, padding=3, bias=False)
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn = nn.BatchNorm2d(self.inplanes)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.bn3 = nn.BatchNorm2d(512)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=3, padding=1)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=5, stride=3, padding=1)
         self.dropout = nn.Dropout2d(p=0.1)
         self.dropout2 = nn.Dropout2d(p=0.15)
         self.dropout3 = nn.Dropout2d(p=0.2)
@@ -138,13 +135,11 @@ class Network(nn.Module):
         x = self.maxpool(x)
         x = self.l1(x)
         x = self.l2(x)
-        # x = self.bn2(x)
         x = self.relu(x)
         x = self.dropout2(x)
-        x = self.maxpool(x)
+        x = self.maxpool2(x)
         x = self.l3(x)
         x = self.l4(x)
-        # x = self.bn3(x)
         x = self.relu(x)
         x = self.dropout3(x)
         x = self.avgpool(x)
